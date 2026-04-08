af1523d4d1bbdf1c373b9cd50b3f0f6ad5c0e048
Tim Lauridsen <tla@rasmil.dk>
2026-04-08 19:41:12 +0200

    + add y=yazi alias

--- fish/.config/fish/conf.d/aliases.fish
+++ fish/.config/fish/conf.d/aliases.fish
@@ -2,3 +2,12 @@
 function lg --wraps lazygit --description 'alias lg=lazygit'
     lazygit $argv
 end
+
+function y --wraps yazi --description 'alias y=yazi'
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
